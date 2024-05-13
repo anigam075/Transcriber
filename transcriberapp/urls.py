8b6645260d3bf2496e7546524d0a9a3992b06bc1
arpit <arpit@onecorp.co.in>
2024-05-13 18:13:06 +0530

    authentication

--- transcriberapp/urls.py
+++ transcriberapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.login_f, name='login'),
     path('logout/', views.logout_f, name='logout'),
     path('register', views.register, name='register'),
+    path('login_homepage', views.login_homepage, name='login_homepage'),
+    # path('login_about', views.login_about, name='login_about'),
 ]
--- transcriberapp/views.py
+++ transcriberapp/views.py
@@ -1,30 +1,63 @@
 from django.shortcuts import render, redirect
 from .models import User_s
-# from django.contrib.auth import login, authenticate
 from .auth import authenticate, login, logout, get_user
 
 def home_f(request):
     return render(request, 'index.html')
 
+def login_homepage(request):
+    if get_user(request) == None:
+        print('Access denied')
+    else:
+        context = {'username' : get_user(request)}
+        return render(request, 'login_index.html', context)
+
+def login_about(request):
+    if get_user(request) == None:
+        print('Access denied')
+    else:
+        context = {'username' : get_user(request)}
+        return render(request, 'about1_login.html', context)
+
 def transcribe_f(request):
-    return render(request, 'about1.html')
+    if get_user(request) == None:
+        context = {'message' : 'Not signed in'}
+        print('Access denied')
+        return render(request, 'login1.html', context)
+    else:
+        context = {'username' : get_user(request)}
+        return render(request, 'about1.html', context)
 
 def category_f(request):
-    return render(request, 'category1.html')
+    if get_user(request) == None:
+        context = {'message' : 'Not signed in'}
+        print('Access denied')
+        return render(request, 'login1.html', context)
+    else:
+        context = {'username' : get_user(request)}
+        return render(request, 'category1.html', context)
 
 def work_f(request):
-    return render(request, 'work1.html')
+    if get_user(request) == None:
+        print('Access denied')
+    else:
+        context = {'username' : get_user(request)}
+        return render(request, 'work1.html', context)
 
 def login_f(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(email=email, password=password)
-        if user:
-            login(request, user)
-            print('user ------- ', user)
-            context = {'request': request}    
-            return redirect('/', context) 
+        if get_user(request) != None:
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                print('user ------- ', user)
+                # context = {'request': get_user(request)}    
+                # return redirect('/login_homepage', context) 
+                return redirect('/login_homepage') 
+        else:
+            print('Already logged in')
     return render(request, 'login1.html')
 
 def logout_f(request):
